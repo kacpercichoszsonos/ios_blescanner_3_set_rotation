3712ac1aac4fbc76965dd51aff9371bdb8121b3e
Kacper Cichosz <kacper.cichosz@sonos.com>
2023-02-06 16:16:33 +0000

    Initial Commit

---
+++ ios_blescanner_3_set_rotation/ios_blescanner_3_set_rotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_blescanner_3_set_rotationApp.swift
+//  ios_blescanner_3_set_rotation
+//
+//  Created by Kacper Cichosz on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_blescanner_3_set_rotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
